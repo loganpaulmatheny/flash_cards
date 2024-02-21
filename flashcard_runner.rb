69e81be50fd3cdfca11d352c17b128809ba11128
Logan Matheny <loganpaulmatheny@gmail.com>
2024-02-21 17:26:12 -0500

    Complete initial CLI

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,7 +2,6 @@ require './lib/card'
 require './lib/deck'
 require './lib/round'
 
-
 card_1 = Card.new("What is the powerhouse of the cell?", "Mitochondria", :Biology)
 
 card_2 = Card.new("Who is known as the father of modern physics?", "Isaac Newton", :Physics)
@@ -13,43 +12,42 @@ card_4 = Card.new("Which planet is known as the Red Planet?", "Mars", :Astronomy
 
 card_5 = Card.new("Who developed the theory of relativity?", "Albert Einstein", :Physics)
 
-card_6 = Card.new("Who was the prime minister of the UK during the majority of WWII?", :History)
+card_6 = Card.new("Who was the prime minister of the UK during the majority of WWII?", "Winston Churchill", :History)
 
 cards = [ card_1, card_2, card_3, card_4, card_5, card_6]
 
 deck = Deck.new(cards)
 
-round = Round(deck)
+$round = Round.new(deck)
 
-def start 
-  while round.current_card_index + 1 <= round.deck.length
+def start
+  round = $round
+  while round.current_card_index + 1 <= round.deck.count
 
     if round.current_card_index == 0
-      puts "Welcome! You're playing with #{deck.length} cards."
+      puts "Welcome! You're playing with #{round.deck.count} cards."
       puts "-------------------------------"
-      puts "This is card number #{round.current_card_index + 1} out of #{round.deck.length}"
-      puts "Question: #{round.current_card}"
+      puts "This is card number #{round.current_card_index + 1} out of #{round.deck.count}"
+      puts "Question: #{round.current_card.question}"
 
       guess = gets.chomp
 
       turn = round.take_turn(guess)
-      puts "#{turn.answer}"
+      puts "#{turn.card.answer}"
       puts "#{turn.feedback}"
-      puts "This is card number #{round.current_card_index + 1} out of #{round.deck.length}"
-      puts "Question: #{round.current_card}"
     else 
-      puts "This is card number #{round.current_card_index + 1} out of #{round.deck.length}"
-      puts "Question: #{round.current_card}"
+      puts "This is card number #{round.current_card_index + 1} out of #{round.deck.count}"
+      puts "Question: #{round.current_card.question}"
       
       guess = gets.chomp
 
       turn = round.take_turn(guess)
-      puts "#{turn.answer}"
+      puts "#{turn.card.answer}"
       puts "#{turn.feedback}"
     end
     
   end  
 
-
-
 end 
+
+start
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,7 @@
 
 class Deck
+include Enumerable
+
   attr_reader :cards
 
   def initialize(cards)
