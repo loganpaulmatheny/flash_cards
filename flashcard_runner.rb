6f6ff269f6831e02de677947ef1e5d6886812ef9
Logan Matheny <loganpaulmatheny@gmail.com>
2024-02-22 10:43:57 -0500

    Complete iteration 3

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -49,10 +49,13 @@ def start
   end  
 
   categories = round.deck.cards.map {|card| card.category}.uniq
-  puts categories
   puts "****** Game Over! ******"
-  puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct}."
-
+  puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct.round(2)}."
+  
+  for category in categories do 
+    puts "#{category} - #{round.percent_correct_by_category(category).round(2)}% correct"
+  end 
+  
   # loop through the categories present in the deck 
 
 
