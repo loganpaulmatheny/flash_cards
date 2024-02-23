5d59390db718ac816020a085d450084516fff95b
Logan Matheny <loganpaulmatheny@gmail.com>
2024-02-23 10:40:06 -0500

    Utilize the card_generator class to create cards from a seperate file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,20 +1,26 @@
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
+# require 'cards'
 
-card_1 = Card.new("What is the powerhouse of the cell?", "Mitochondria", :Biology)
+# card_1 = Card.new("What is the powerhouse of the cell?", "Mitochondria", :Biology)
 
-card_2 = Card.new("Who is known as the father of modern physics?", "Isaac Newton", :Physics)
+# card_2 = Card.new("Who is known as the father of modern physics?", "Isaac Newton", :Physics)
 
-card_3 = Card.new("What is the chemical symbol for water?", "H2O", :Chemistry)
+# card_3 = Card.new("What is the chemical symbol for water?", "H2O", :Chemistry)
 
-card_4 = Card.new("Which planet is known as the Red Planet?", "Mars", :Astronomy)
+# card_4 = Card.new("Which planet is known as the Red Planet?", "Mars", :Astronomy)
 
-card_5 = Card.new("Who developed the theory of relativity?", "Albert Einstein", :Physics)
+# card_5 = Card.new("Who developed the theory of relativity?", "Albert Einstein", :Physics)
 
-card_6 = Card.new("Who was the prime minister of the UK during the majority of WWII?", "Winston Churchill", :History)
+# card_6 = Card.new("Who was the prime minister of the UK during the majority of WWII?", "Winston Churchill", :History)
 
-cards = [ card_1, card_2, card_3, card_4, card_5, card_6]
+# cards = [ card_1, card_2, card_3, card_4, card_5, card_6]
+
+filename = 'cards.txt'
+
+cards = CardGenerator.new(filename).cards
 
 deck = Deck.new(cards)
 
